ed1d63a38ab709e9d866a983b017151aaa0fa815
phamr39 <pha.mr3998@gmail.com>
2022-06-14 22:40:08 +0700

    upgrade near-sdk version, edit apikey

--- test_cmd.sh
+++ test_cmd.sh
@@ -1,12 +1,13 @@
-near deploy --wasmFile res/neariot_contract.wasm --accountId neariot.testnet
+yarn build && near deploy --wasmFile res/neariot_contract.wasm --accountId neariot.testnet
 
-near call ciuz.testnet new "{}" --accountId ciuz.testnet
+near call ciuz.testnet new "{}" --accountId neariot.testnet
 
 near call neariot.testnet new_cluster '{"name":"aloha","description":"Hello World"}' --accountId neariot.testnet
 
 near call neariot.testnet get_clusters --accountId neariot.testnet
 
-near call neariot.testnet get_cluster '{"id":"Y2l1ei50ZXN0bmV0XzE2NTM1ODAyMjgzMTE5OTkxMTE="}' --accountId neariot.testnet
+near call neariot.testnet get_cluster '{"id":"bmVhcmlvdC50ZXN0bmV0XzE2NTUyMTg5NzI1OTM1NDUyODU="}' --accountId neariot.testnet
 
-near call neariot.testnet get_cluster_data '{"id":"Y2l1ei50ZXN0bmV0XzE2NTM1ODAyMjgzMTE5OTkxMTE="}' --accountId neariot.testnet
+near call neariot.testnet get_cluster_data '{"id":"bmVhcmlvdC50ZXN0bmV0XzE2NTUyMTg5NzI1OTM1NDUyODU="}' --accountId neariot.testnet
 
+near call neariot.testnet set_apikey_hash '{"id":"bmVhcmlvdC50ZXN0bmV0XzE2NTUyMTg5NzI1OTM1NDUyODU=", "apikey_hash":"Abchasdasjkjl"}' --accountId neariot.testnet
